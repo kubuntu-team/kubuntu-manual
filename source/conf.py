bb18981323d7c463b62ba4c0d7432e7cc866198e
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2016-08-02 15:19:07 -0400

    changed copyright to end of this LTS

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kubuntu Manual'
-copyright = u'2017, Kubuntu Team'
+copyright = u'2021, Kubuntu Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
